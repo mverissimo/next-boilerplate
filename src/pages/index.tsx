6a158c987d190307d4ae41b97ecea8123c96aa17
mverissimo <mverissimoo@hotmail.com>
2021-06-21 10:46:56 -0300

    feat(document): add meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,16 @@ export default function Home() {
   return (
     <React.Fragment>
       <Head>
-        <title>Create Next App</title>
+        <title>{process.env.SITE_TITLE}</title>
+
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, minimum-scale=1"
+        />
+        <meta name="description" content={process.env.SITE_DESCRIPTION} />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
