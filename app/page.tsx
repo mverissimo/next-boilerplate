97445dddb0aeb3abb98c6d7bd33eb7490f74e8f9
mverissimo <mverissimoo@hotmail.com>
2022-11-18 23:49:54 -0300

    build(tailwind): update paths

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Button } from '@/components/Button';
+import { Button } from '@/ui/button';
 
 export default function Home() {
   return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
-  content: ['./src/app/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  content: ['./app/**/*.{ts,tsx}', './ui/**/*.{ts,tsx}'],
   theme: {
     extend: {},
   },
